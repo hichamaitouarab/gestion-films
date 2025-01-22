52167a0d94646d17f7d256c766982f76c7bb0fd8
hichamaitouarab <99011793+hichamaitouarab@users.noreply.github.com>
2025-01-22 20:48:24 +0100

    Ajout du bouton menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
-import { Routes, Route } from 'react-router-dom';
-import Accueil from './pages/Accueil';
-import Recherche from './pages/Recherche';
-import AjouterFilm from './pages/AjouterFilm';
-import DetailsFilm from './pages/DetailsFilm';
+import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import Accueil from "./pages/Accueil";
+import Recherche from "./pages/Recherche";
+import AjouterFilm from "./pages/AjouterFilm";
+import DetailsFilm from "./pages/DetailsFilm";
+import "./App.css";
 
 const App = () => {
+  const [menuVisible, setMenuVisible] = useState(true);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Accueil />} />
-      <Route path="/recherche" element={<Recherche />} />
-      <Route path="/ajouter" element={<AjouterFilm />} />
-      <Route path="/film/:id" element={<DetailsFilm />} />
-    </Routes>
+    <div className="app-container">
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      {/* Menu latéral */}
+      {menuVisible && (
+        <nav className="sidebar">
+          <h2></h2>
+          <ul>
+            <li>
+              <Link to="/">Accueil</Link>
+            </li>
+            <li>
+              <Link to="/recherche">Recherche</Link>
+            </li>
+            <li>
+              <Link to="/ajouter">Ajouter un Film</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/recherche" element={<Recherche />} />
+          <Route path="/ajouter" element={<AjouterFilm />} />
+          <Route path="/film/:id" element={<DetailsFilm />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css"; 
+import "./App.css"; 
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
